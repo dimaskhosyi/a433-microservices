3a83e9dcfc820220bdccc55930257dda1e032355
Dimas Khosyi Triangga <64787626+dimaskhosyi@users.noreply.github.com>
2023-06-11 14:36:50 +0700

    Add Script Build & Push Image

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,12 +1,12 @@
-# Memberi tahu sistem, bahwa file dijalankan menggunakan shell
+# Mengenalkan ke sistem, bahwa file dijalankan menggunakan shell
 #!/bin/bash
 
-# Melakukan build image docker file dengan nama todo-app dan tag v1
+# Melakukan build image docker file dengan nama dam tag sebagai "ghcr.io/dimaskhosyi/karsajobs:latest"
 docker build . -t ghcr.io/dimaskhosyi/karsajobs:latest
 
-# Menyimpan token github, dan melakukan login ke Github Packages
+# Menyimpan token github, dan melakukan login ke Github Packages menggunakan tokens yang telah disimpan
 export CR_PAT=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
 echo $CR_PAT | docker login ghcr.io -u dimaskhosyi --password-stdin
 
-# Mengunggah image ke Github Packages
+# Mengunggah hasil image ke Github Packages
 docker push ghcr.io/dimaskhosyi/karsajobs:latest
